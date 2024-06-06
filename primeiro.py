0bf07232e91b42fc56fc4911cd3f84328f6362e1
isjoaovictor <joaovicto222@hotmail.com>
2024-06-06 20:54:47 -0300

    teste

---
+++ primeiro.py
@@ -0,0 +1,18 @@
+import pyautogui as pygui
+
+
+while True:
+    try:
+      peso = float(input("qual seu peso: "))
+      alt = float(input("qual sua altura: "))
+      imc = peso/(alt*alt)
+      if imc > 30:
+       print("gordo")
+      print(f'{imc:.2f}')
+      break
+    except ValueError:
+       print('valor invalido')
+
+
+
+
